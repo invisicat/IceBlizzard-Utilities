a131923e21a1aaa72b6527d2fca35e4d2e06f80c
douglas <iceblizzard10000@gmail.com>
2021-05-14 01:26:16 -0400

    fixed plugin yml

--- src/main/java/dev/blizzardutils/example/ExamplePlugin.java
+++ src/main/java/dev/blizzardutils/example/ExamplePlugin.java
@@ -14,13 +14,19 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiConsumer;
 
 public class ExamplePlugin extends JavaPlugin {
 
 
     private final List<String> lore = Arrays.asList("&eThis is a fire snowball", "&c1, 2, 3");
+    private final KronosLibrary<Void> kronosLibrary = new KronosLibrary<>();
     private final Map<Enchantment, Integer> enchantMap = new ConcurrentHashMap<>();
 
+
+    public void onEnable(){
+
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -40,6 +46,11 @@ public class ExamplePlugin extends JavaPlugin {
             enchantMap.put(Enchantment.DURABILITY, 5);
             player.getInventory().addItem(ItemBuilder.Builder.getInstance().itemType(Material.SNOW_BALL).itemAmount(1).itemName(StringUtils.format("&6Snowball"))
                     .itemEnchant(enchantMap).itemLore(lore).buildWithEnchants());
+            kronosLibrary.createNewChain(null).runAsync(() -> {
+                player.sendMessage("This is an Async Task of Kronos Chain");
+            }).whenCompleteSync((aVoid, throwable) -> {
+                kronosLibrary.callBukkitSyncTask(this, () -> player.sendMessage("Now Running Bukkit Sync Task After Kronos Chain running async"));
+            });
         }
         return true;
     }
